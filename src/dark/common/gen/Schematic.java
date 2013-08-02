98787fe6acaa9989c502cccb9272578b0d9edb24
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-02 01:54:34 -0400

    AutoCommit

--- src/dark/common/gen/Schematic.java
+++ src/dark/common/gen/Schematic.java
@@ -1,14 +1,46 @@
 package dark.common.gen;
 
+import java.io.File;
+import java.io.InputStream;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+
 public class Schematic
 {
-    public Schematic(String location, String fileName)
+    String fileName;
+    short width,height,length;
+    byte[] blocks,data;
+
+    public Schematic(String fileName)
     {
 
     }
 
     public Schematic load()
     {
+
+        try
+        {
+            InputStream fis = getClass().getResourceAsStream(fileName + ".schematic");
+            NBTTagCompound nbtdata = NBTCompressedStreamTools(fis);
+
+            width = nbtdata.getShort("Width");
+            height = nbtdata.getShort("Height");
+            length = nbtdata.getShort("Length");
+
+            blocks = nbtdata.getByteArray("Blocks");
+            data = nbtdata.getByteArray("Data");
+
+            //NBTTagList entities = nbtdata.getTagList("Entities");
+            //NBTTagList tileentities = nbtdata.getTagList("TileEntities");
+
+            fis.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
         return this;
     }
 }
