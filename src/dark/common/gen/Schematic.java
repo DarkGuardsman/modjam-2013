94b53d45a9fdd3ffbfbba0aea5c4dec71584d107
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-02 04:09:34 -0400

    AutoCommit_1

--- src/dark/common/gen/Schematic.java
+++ src/dark/common/gen/Schematic.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+import dark.common.prefab.Pos;
 import dark.common.prefab.PosWorld;
 
 import net.minecraft.nbt.CompressedStreamTools;
@@ -27,7 +28,6 @@ public class Schematic
 
         try
         {
-            ResourceLocation location;
             File file = new File(Schematic.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParentFile();
             InputStream fis = new FileInputStream(file.getPath() + File.separator + "fireflower.schematic");
             NBTTagCompound nbtdata = CompressedStreamTools.readCompressed(fis);
@@ -40,7 +40,7 @@ public class Schematic
             data = nbtdata.getByteArray("Data");
             for (int i = 0; i < blocks.length; i++)
             {
-                System.out.println("BlockByte" + i + ":" + blocks[i]);
+                System.out.println("BlockByte" + i + ":" + blocks[i] +"  "+data[i]);
             }
             //NBTTagList entities = nbtdata.getTagList("Entities");
             //NBTTagList tileentities = nbtdata.getTagList("TileEntities");
@@ -56,6 +56,30 @@ public class Schematic
 
     public void build(PosWorld location)
     {
-
+        Pos start = new Pos(location.xx + (width/2), Math.min(location.yy + height, 255), location.zz + (length/2));
+        Pos end = new Pos(location.xx - (width/2), Math.max(location.yy, 0), location.zz - (length/2));
+        int i = 0;
+        int x, y, z;
+        for (y = start.y(); y <= start.y() && y >= end.y(); y--)
+        {
+            for (x = start.x(); x <= start.x() && x >= end.x(); x--)
+            {
+                for (z = start.z(); z <= start.z() && z >= end.z(); z--)
+                {
+                    i++;
+                    int b = 0;
+                    int m = 0;
+                    if(i < blocks.length)
+                    {
+                        b = blocks[i];
+                    }
+                    if(i <data.length)
+                    {
+                        m = data[i];
+                    }
+                    location.world.setBlock(x, y, z, b,m,2);
+                }
+            }
+        }
     }
 }
