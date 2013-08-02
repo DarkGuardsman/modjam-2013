be7f172a69b61c2e1b675d6401d61c7c8857b24d
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-01 22:08:03 -0400

    Start on the bot prefab

---
+++ src/dark/common/entity/EntityBot.java
@@ -0,0 +1,30 @@
+package dark.common.entity;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.world.World;
+
+public class EntityBot extends EntityLiving implements IHiveObject
+{
+    protected int hiveID = -1;
+    public EntityBot(World par1World)
+    {
+        super(par1World);
+    }
+
+    @Override
+    public void setHiveID(int id)
+    {
+        this.hiveID = id;
+
+    }
+    @Override
+    public int getHiveID()
+    {
+       if(this.hiveID == -1)
+       {
+
+       }
+        return 0;
+    }
+
+}
--- src/dark/common/prefab/PosWorld.java
+++ src/dark/common/prefab/PosWorld.java
@@ -1,12 +1,20 @@
 package dark.common.prefab;
 
+import net.minecraft.world.World;
+
 public class PosWorld extends Pos
 {
+    World world;
 
-    public PosWorld(double x, double y, double z)
+    public PosWorld(World world, Pos pos)
+    {
+        this(world, pos.xx, pos.yy, pos.zz);
+    }
+
+    public PosWorld(World world, double x, double y, double z)
     {
         super(x, y, z);
-        // TODO Auto-generated constructor stub
+        this.world = world;
     }
 
 }
