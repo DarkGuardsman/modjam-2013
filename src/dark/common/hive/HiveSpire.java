61ff64afc36ce76a245a314c35709324e1543780
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-02 00:24:34 -0400

    AutoCommit

--- src/dark/common/hive/HiveSpire.java
+++ src/dark/common/hive/HiveSpire.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import dark.common.api.IHiveSpire;
 import dark.common.prefab.PosWorld;
@@ -15,8 +16,13 @@ public class HiveSpire implements IHiveSpire
     PosWorld location;
     Hivemind hivemind;
     String hiveName = "world";
+    int side = 1;
+    List<IInventory> inventory = new ArrayList<IInventory>();
 
-    HashMap<ItemStack, Integer> containedItems = new HashMap<ItemStack, Integer>();
+    public void scanArea()
+    {
+
+    }
 
     @Override
     public Hivemind getHive()
@@ -68,7 +74,7 @@ public class HiveSpire implements IHiveSpire
             ItemStack stack = stackIn.copy();
             stack.stackSize = 1;
             int a = stackIn.stackSize;
-            if(this.containedItems.containsKey(stack))
+            if (this.containedItems.containsKey(stack))
             {
                 a += this.containedItems.get(stack);
             }
