a5fa1fd95d6bdfa50335a8c3149d122e5d95cdfb
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-02 18:47:54 -0400

    Getting food

--- src/dark/common/gen/DarkSchematic.java
+++ src/dark/common/gen/DarkSchematic.java
@@ -1,14 +1,27 @@
 package dark.common.gen;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 
+import net.minecraft.nbt.CompressedStreamTools;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
-
+import dark.common.prefab.Pair;
 import dark.common.prefab.Pos;
 
 public class DarkSchematic
 {
     public HashMap<Pos, Pair<Integer, Integer>> blocks = new HashMap<>();
+
+    String fileName;
+
+    public DarkSchematic(String fileName)
+    {
+        fileName = fileName;
+    }
 
     public void loadWorldSelection(World world, Pos pos, Pos pos2)
     {
@@ -46,10 +59,37 @@ public class DarkSchematic
                 {
                     int blockID = world.getBlockId(start.x() + x, start.y() + y, start.z() + z);
                     int blockMeta = world.getBlockMetadata(start.x() + x, start.y() + y, start.z() + z);
-                    blocks.put(new Pos(x, y, z), new Pair<Integer, Integer>(blockID,blockMeta ));
+                    blocks.put(new Pos(x, y, z), new Pair<Integer, Integer>(blockID, blockMeta));
                 }
             }
         }
 
+    }
+
+    public DarkSchematic load()
+    {
+        try
+        {
+            File file = new File(McEditSchematic.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParentFile();
+            NBTTagCompound nbtdata = CompressedStreamTools.readCompressed(new FileInputStream(new File(file, fileName + ".sch")));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    public void save()
+    {
+        try
+        {
+            File file = new File(McEditSchematic.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParentFile();
+            NBTTagCompound nbtdata = CompressedStreamTools.readCompressed(new FileInputStream(new File(file, fileName + ".sch")));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
