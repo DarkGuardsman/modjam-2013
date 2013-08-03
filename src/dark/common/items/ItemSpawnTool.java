4fc2547f28ff153c9a3ef10d791998b23564be20
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-03 04:44:02 -0400

    Made Schematic writer look like a pencil

--- src/dark/common/items/ItemSpawnTool.java
+++ src/dark/common/items/ItemSpawnTool.java
@@ -5,11 +5,16 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatMessageComponent;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import dark.common.DarkBotMain;
 import dark.common.gen.DarkSchematic;
@@ -18,6 +23,9 @@ import dark.common.prefab.PosWorld;
 
 public class ItemSpawnTool extends Item
 {
+    static Icon wand;
+    static Icon place;
+
     boolean flip = false;
     Pos pos;
     Pos pos2;
@@ -27,6 +35,30 @@ public class ItemSpawnTool extends Item
         super(DarkBotMain.config.getItem("SpawnTool", par).getInt());
         this.setUnlocalizedName("SpawnTool");
         this.setCreativeTab(CreativeTabs.tabRedstone);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public Icon getIconFromDamage(int par1)
+    {
+        if(par1 == 0)
+        {
+            return wand;
+        }
+        if(par1 == 1)
+        {
+            return place;
+        }
+        return this.itemIcon;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerIcons(IconRegister par1IconRegister)
+    {
+        super.registerIcons(par1IconRegister);
+        wand = par1IconRegister.registerIcon(DarkBotMain.PREFIX + "WorldEditWand");
+        place = par1IconRegister.registerIcon(DarkBotMain.PREFIX + "WorldEditPlacer");
     }
 
     @Override
@@ -48,7 +80,7 @@ public class ItemSpawnTool extends Item
                 }
                 if (pos2 != null && pos != null)
                 {
-                    DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd");
+                    DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_hh.mm.ss");
                     new DarkSchematic("Schematic_" + dateFormat.format(new Date())).loadWorldSelection(world, pos, pos2).save();
                     player.sendChatToPlayer(ChatMessageComponent.func_111066_d("Saved Schematic"));
                     pos = null;
