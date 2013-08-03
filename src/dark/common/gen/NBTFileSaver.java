275d08b167d8b771a88d37a6b71f0d9e0421de3a
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-02 20:54:34 -0400

    AutoCommit_1

--- src/dark/common/gen/NBTFileSaver.java
+++ src/dark/common/gen/NBTFileSaver.java
@@ -1,18 +1,74 @@
 package dark.common.gen;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.nbt.CompressedStreamTools;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.FMLLog;
 
 public class NBTFileSaver
 {
 
-    public File getSaveDirectory()
+    public static boolean saveNBTFile(String filename, File direcotry, NBTTagCompound data)
     {
-        File saveDirectory = getSaveDirectory(MinecraftServer.getServer().getFolderName());
+        try
+        {
+            File tempFile = new File(direcotry, filename + "_tmp.dat");
+            File file = new File(direcotry, filename + ".dat");
+
+            CompressedStreamTools.writeCompressed(data, new FileOutputStream(tempFile));
+
+            if (file.exists())
+            {
+                file.delete();
+            }
+
+            tempFile.renameTo(file);
+            return true;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean saveNBTFile(String filename, NBTTagCompound data)
+    {
+        return saveNBTFile(filename, getWorldSaveFolder(MinecraftServer.getServer().getFolderName()), data);
+    }
+
+    public static NBTTagCompound getSaveFile(File saveDirectory, String filename)
+    {
+        try
+        {
+            File file = new File(saveDirectory, filename + ".dat");
+
+            if (file.exists())
+            {
+                return CompressedStreamTools.readCompressed(new FileInputStream(file));
+            }
+            else
+            {
+                return new NBTTagCompound();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public File getSaveFolder()
+    {
+        File saveDirectory = getWorldSaveFolder(MinecraftServer.getServer().getFolderName());
         File file = new File(saveDirectory, "dark");
 
         if (!saveDirectory.exists())
@@ -28,19 +84,19 @@ public class NBTFileSaver
         return file;
     }
 
-    public static File getSaveDirectory(String worldName)
+    public static File getWorldSaveFolder(String worldName)
     {
-        File parent = getBaseDirectory();
+        File parent = getBaseFolder();
 
         if (FMLCommonHandler.instance().getSide().isClient())
         {
-            parent = new File(getBaseDirectory(), "saves" + File.separator);
+            parent = new File(getBaseFolder(), "saves" + File.separator);
         }
 
         return new File(parent, worldName + File.separator);
     }
 
-    public static File getBaseDirectory()
+    public static File getBaseFolder()
     {
         if (FMLCommonHandler.instance().getSide().isClient())
         {
