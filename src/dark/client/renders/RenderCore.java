5eecb12ce1342add5dc3fb2c8f1f3bc6150295be
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-03 07:24:40 -0400

    Auto Commit Sat 08/03/2013- 7:24:36.25

--- src/dark/client/renders/RenderCore.java
+++ src/dark/client/renders/RenderCore.java
@@ -39,11 +39,11 @@ public class RenderCore extends TileEntitySpecialRenderer
     {
 
         GL11.glPushMatrix();
-        GL11.glTranslated(xx + xChange + .54, yy + yChange + 2.2, zz + zChange + .54);
+        GL11.glTranslated(xx + xChange + .50, yy + yChange + 2.2, zz + zChange + .50);
         GL11.glDisable(GL11.GL_CULL_FACE);
         GL11.glScalef(s, s, s);
         GL11.glRotatef(r, 0F, 1F, 0F);
-        r += .001;
+        r += 2;
 
         FMLClientHandler.instance().getClient().renderEngine.func_110577_a(new ResourceLocation(DarkBotMain.DOMAIN, "textures/uv/" + texture + ".png"));
         modelTurret.renderAll();
