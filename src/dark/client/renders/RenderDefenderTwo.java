a22983298106325bfeaec4fbafc65230e280da3c
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-05 05:39:34 -0400

    Auto Commit Mon 08/05/2013- 5:39:34.29

---
+++ src/dark/client/renders/RenderDefenderTwo.java
@@ -0,0 +1,32 @@
+package dark.client.renders;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderDefenderTwo extends RenderLiving
+{
+
+    public RenderDefenderTwo(ModelBase par1ModelBase, float par2)
+    {
+        super(par1ModelBase, par2);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void doRender(Entity entity, double d0, double d1, double d2, float f, float f1)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected ResourceLocation func_110775_a(Entity entity)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
