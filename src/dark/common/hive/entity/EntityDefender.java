02ef222f39369e31f7f1055b7742f92627389a43
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-05 09:59:54 -0400

    Added ranged attack to drone

--- src/dark/common/hive/entity/EntityDefender.java
+++ src/dark/common/hive/entity/EntityDefender.java
@@ -11,6 +11,8 @@ import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntitySmallFireball;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
@@ -195,13 +197,55 @@ public class EntityDefender extends EntityCreature implements IHiveObject
     }
 
     @Override
-    protected void attackEntity(Entity par1Entity, float par2)
+    protected void attackEntity(Entity attackTarget, float range)
     {
-        //TODO add ranged attack
-        if (this.attackTime <= 0 && par2 < 2.0F && par1Entity.boundingBox.maxY > this.boundingBox.minY && par1Entity.boundingBox.minY < this.boundingBox.maxY)
+        if (this.attackTime <= 0 && range < 2.0F && attackTarget.boundingBox.maxY > this.boundingBox.minY && attackTarget.boundingBox.minY < this.boundingBox.maxY)
         {
             this.attackTime = 20;
-            this.attackEntityAsMob(par1Entity);
+            this.attackEntityAsMob(attackTarget);
+        }
+        else if (range < 30.0F)
+        {
+            double d0 = attackTarget.posX - this.posX;
+            double d1 = attackTarget.boundingBox.minY + (double)(attackTarget.height / 2.0F) - (this.posY + (double)(this.height / 2.0F));
+            double d2 = attackTarget.posZ - this.posZ;
+
+            if (this.attackTime == 0)
+            {
+                ++this.field_70846_g;
+
+                if (this.field_70846_g == 1)
+                {
+                    this.attackTime = 60;
+                    this.func_70844_e(true);
+                }
+                else if (this.field_70846_g <= 4)
+                {
+                    this.attackTime = 6;
+                }
+                else
+                {
+                    this.attackTime = 100;
+                    this.field_70846_g = 0;
+                    this.func_70844_e(false);
+                }
+
+                if (this.field_70846_g > 1)
+                {
+                    float f1 = MathHelper.sqrt_float(range) * 0.5F;
+                    this.worldObj.playAuxSFXAtEntity((EntityPlayer)null, 1009, (int)this.posX, (int)this.posY, (int)this.posZ, 0);
+
+                    for (int i = 0; i < 1; ++i)
+                    {
+                        EntitySmallFireball entitysmallfireball = new EntitySmallFireball(this.worldObj, this, d0 + this.rand.nextGaussian() * (double)f1, d1, d2 + this.rand.nextGaussian() * (double)f1);
+                        entitysmallfireball.posY = this.posY + (double)(this.height / 2.0F) + 0.5D;
+                        this.worldObj.spawnEntityInWorld(entitysmallfireball);
+                    }
+                }
+            }
+
+            this.rotationYaw = (float)(Math.atan2(d2, d0) * 180.0D / Math.PI) - 90.0F;
+            this.hasAttacked = true;
         }
     }
 
