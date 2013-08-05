62d4177e68dc4ebf22323b7fb8743be430d7604f
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-05 05:54:34 -0400

    Auto Commit Mon 08/05/2013- 5:54:34.29

--- src/dark/client/renders/RenderDefenderTwo.java
+++ src/dark/client/renders/RenderDefenderTwo.java
@@ -1,32 +1,46 @@
 package dark.client.renders;
 
-import net.minecraft.client.model.ModelBase;
+import org.lwjgl.opengl.GL11;
+
+import cpw.mods.fml.client.FMLClientHandler;
+import dark.common.DarkBotMain;
 import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
 
-public class RenderDefenderTwo extends RenderLiving
+public class RenderDefenderTwo extends Render
 {
+    private IModelCustom model;
 
-    public RenderDefenderTwo(ModelBase par1ModelBase, float par2)
+    public RenderDefenderTwo()
     {
-        super(par1ModelBase, par2);
-        // TODO Auto-generated constructor stub
+        model = AdvancedModelLoader.loadModel("/assets/dark/models/Drone.Wheel.obj");
     }
-
     @Override
-    public void doRender(Entity entity, double d0, double d1, double d2, float f, float f1)
+    public void doRender(Entity entity, double xx, double yy, double zz, float f, float f1)
     {
-        // TODO Auto-generated method stub
+        GL11.glPushMatrix();
+        GL11.glTranslated(xx + .50, yy + 2.2, zz + .50);
+        GL11.glDisable(GL11.GL_CULL_FACE);
+        GL11.glScalef(1, 1, 1);
+        GL11.glRotatef(entity.rotationYaw, 0F, 1F, 0F);
+
+        FMLClientHandler.instance().getClient().renderEngine.func_110577_a(this.func_110775_a(entity));
+        model.renderAll();
+
+
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glPopMatrix();
+
 
     }
 
     @Override
     protected ResourceLocation func_110775_a(Entity entity)
     {
-        // TODO Auto-generated method stub
-        return null;
+        return new ResourceLocation(DarkBotMain.DOMAIN, "uv/Drone.Wheel.png");
     }
 
 }
