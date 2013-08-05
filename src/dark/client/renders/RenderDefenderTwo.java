580932585bdba79f2384e1b671864cc28dda9a72
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-05 07:24:34 -0400

    Auto Commit Mon 08/05/2013- 7:24:34.49

--- src/dark/client/renders/RenderDefenderTwo.java
+++ src/dark/client/renders/RenderDefenderTwo.java
@@ -1,23 +1,33 @@
 package dark.client.renders;
 
-import org.lwjgl.opengl.GL11;
-
-import cpw.mods.fml.client.FMLClientHandler;
-import dark.common.DarkBotMain;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.AdvancedModelLoader;
 import net.minecraftforge.client.model.IModelCustom;
 
+import org.lwjgl.opengl.GL11;
+
+import cpw.mods.fml.client.FMLClientHandler;
+import dark.common.DarkBotMain;
+
 public class RenderDefenderTwo extends Render
 {
-    private IModelCustom model;
+    private IModelCustom modelBody;
+    private IModelCustom modelTrack;
+    private IModelCustom modelWheel;
+    private ResourceLocation body = new ResourceLocation(DarkBotMain.DOMAIN, "textures/uv/Drone.Wheel.Body.png");
+    private ResourceLocation track = new ResourceLocation(DarkBotMain.DOMAIN, "textures/uv/Drone.Wheel.Track.png");
+    private ResourceLocation wheel = new ResourceLocation(DarkBotMain.DOMAIN, "textures/uv/Drone.Wheel.Wheel.png");
 
     public RenderDefenderTwo()
     {
-        model = AdvancedModelLoader.loadModel("/assets/dark/models/Drone.Wheel.obj");
+        modelBody = AdvancedModelLoader.loadModel("/assets/dark/models/Drone.Wheel.Body.obj");
+        modelTrack = AdvancedModelLoader.loadModel("/assets/dark/models/Drone.Wheel.Track.obj");
+        modelWheel = AdvancedModelLoader.loadModel("/assets/dark/models/Drone.Wheel.Wheel.obj");
     }
+
     @Override
     public void doRender(Entity entity, double xx, double yy, double zz, float f, float f1)
     {
@@ -25,15 +35,23 @@ public class RenderDefenderTwo extends Render
         GL11.glTranslated(xx, yy, zz);
         GL11.glDisable(GL11.GL_CULL_FACE);
         GL11.glScalef(1.5f, 1.5f, 1.5f);
-        GL11.glRotatef(entity.rotationYaw - 90, 0F, 1F, 0F);
+        float rotation = entity.rotationYaw - 90;
+        GL11.glRotatef(rotation, 0F, 1F, 0F);
 
-        FMLClientHandler.instance().getClient().renderEngine.func_110577_a(this.func_110775_a(entity));
-        model.renderAll();
+        FMLClientHandler.instance().getClient().renderEngine.func_110577_a(body);
+        modelBody.renderAll();
+        if (entity instanceof EntityLiving)
+        {
+            GL11.glRotatef(rotation - entity.r, 0F, 1F, 0F);
+        }
+        FMLClientHandler.instance().getClient().renderEngine.func_110577_a(track);
+        modelTrack.renderAll();
 
+        FMLClientHandler.instance().getClient().renderEngine.func_110577_a(wheel);
+        modelWheel.renderAll();
 
         GL11.glEnable(GL11.GL_CULL_FACE);
         GL11.glPopMatrix();
-
 
     }
 
